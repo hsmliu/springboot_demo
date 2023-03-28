189c9ad3b109dc6b4fbb5ebe1d77c03a5b8b03d6
Liu wencheng <lwcjobs@163.com>
2023-03-28 18:04:52 +0800

    Update RedisConfig.java
    修改注释

--- springboot_redis/src/main/java/com/example/springboot_redis/config/RedisConfig.java
+++ springboot_redis/src/main/java/com/example/springboot_redis/config/RedisConfig.java
@@ -12,7 +12,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    // 这是我给大家写好的一个固定模板，大家在企业中，拿去就可以直接使用！
     // 自定义一个RedisTemplate
     @Bean
     @SuppressWarnings("all")
